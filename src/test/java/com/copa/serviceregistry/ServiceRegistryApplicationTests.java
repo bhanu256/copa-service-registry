2b8328363d7512e9735758876192e48d5cd5985b
bhanu256 <bhanuphani100@gmail.com>
2022-03-29 00:37:30 +0530

    initial commit

---
+++ src/test/java/com/copa/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.copa.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
